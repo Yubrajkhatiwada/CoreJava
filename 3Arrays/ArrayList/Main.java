4bf3040ae6def2a2c4849a6e97d72a35a22c434e
Yubraj Khatiwada <121674369+Yubrajkhatiwada@users.noreply.github.com>
2025-02-09 18:33:12 +0545

    Update Main.java

--- 3Arrays/ArrayList/Main.java
+++ 3Arrays/ArrayList/Main.java
@@ -5,8 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 			
 		// ArrayList = 	a resizable array. 
-		//				Elements can be added and removed after compilation phase
-		//				store reference data types
+		//Elements can be added and removed after compilation phase
+		//store reference data types
 		
 		ArrayList<String> food = new ArrayList<String>();
 		
