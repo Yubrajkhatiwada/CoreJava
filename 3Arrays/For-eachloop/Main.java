8b950935f101c6bdefa82af821b28df925c69604
Yubraj Khatiwada <121674369+Yubrajkhatiwada@users.noreply.github.com>
2025-02-09 18:45:04 +0545

    Update Main.java

--- 3Arrays/For-eachloop/Main.java
+++ 3Arrays/For-eachloop/Main.java
@@ -5,8 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 			
 		// for-each = 	traversing technique to iterate through the elements in an array/collection
-		//				less steps, more readable
-		//				less flexible
+		//less steps, more readable
+		//less flexible
 		
 		//String[] animals = {"cat","dog","rat","bird"};
 		ArrayList<String> animals = new ArrayList<String>();
